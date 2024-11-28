267d56faefa03ab8555b81b68033770518e81d12
Augustas Stankūnas <149320290+AugustasStankunas@users.noreply.github.com>
2024-11-28 12:17:18 +0200

    Negrazus kodas, bet veikia

--- automaticPlanning.py
+++ automaticPlanning.py
@@ -4,6 +4,7 @@ import arcpy
 import numpy as np
 import math
 import heapq
+import copy
 
 class Toolbox:
     def __init__(self):
@@ -85,17 +86,37 @@ class Tool:
 
         #konfiguracija
         current_map = arcpy.mp.ArcGISProject('CURRENT').activeMap
-        spatial_reference = current_map.spatialReference
-        #arcpy.AddMessage(spatial_reference)
-        #lineFeatureClass = arcpy.CreateFeatureclass_management(arcpy.env.workspace,"Line","POLYLINE", spatial_reference = spatial_reference)
+        spatial_reference = arcpy.SpatialReference(3346)
+        lineFeatureClass = arcpy.CreateFeatureclass_management(arcpy.env.workspace,"Line","POLYLINE", spatial_reference = spatial_reference)
+        pointFeatureClass = arcpy.CreateFeatureclass_management(arcpy.env.workspace,"Points","POINT", spatial_reference = spatial_reference)
+
+        
+
 
         boolArray, startIndexes, endIndexes = self.createBoolArray(startPoint, endPoint)
         arcpy.AddMessage(boolArray)
+        arcpy.AddMessage(startIndexes)
+        arcpy.AddMessage(endIndexes)
 
         path = a_star(boolArray, startIndexes, endIndexes)
+        if path is None:
+            arcpy.AddMessage("No path found")
+            return
         arcpy.AddMessage(path)
 
-        #current_map.addDataFromPath(lineFeatureClass)
+        pointArray = self.createPointArray(startPoint, path, startIndexes)
+
+        with arcpy.da.InsertCursor("Points", ["SHAPE@"]) as cursor:
+            for point in pointArray:
+                cursor.insertRow([point])
+
+        line = arcpy.Polyline(pointArray, spatial_reference = spatial_reference)
+
+        with arcpy.da.InsertCursor("Line", ["SHAPE@"]) as cursor:
+            cursor.insertRow([line])
+
+        current_map.addDataFromPath(lineFeatureClass)
+        current_map.addDataFromPath(pointFeatureClass)
         return
 
     def postExecute(self, parameters):
@@ -105,25 +126,76 @@ class Tool:
     
 
     def createBoolArray(self, start: arcpy.Point, end: arcpy.Point):
-        #starting width and height
+        start = snapPointToRasterCenter(self, start)
+        end = snapPointToRasterCenter(self, end)
         width = abs(start.X - end.X)
+        arcpy.AddMessage(f'Width: {width}')
         height = abs(start.Y - end.Y)
-        numberOfColumns = math.ceil(width / self.resolution)
-        numberOfRows = math.ceil(height / self.resolution)
-        paddingColumns = math.ceil(0.5 * width / self.resolution)
-        paddingRrows = math.ceil(0.5 * height / self.resolution)
+        arcpy.AddMessage(f'Height: {height}')
+        numberOfColumns = math.ceil(width / self.resolution) + 1
+        arcpy.AddMessage(f'Number of columns: {numberOfColumns}')
+        numberOfRows = math.ceil(height / self.resolution) + 1
+        testArray = arcpy.RasterToNumPyArray(self.raster, start, numberOfColumns, numberOfRows)
+        arcpy.AddMessage(testArray)
+        arcpy.AddMessage(f'Number of rows: {numberOfRows}')
+        paddingColumns = math.ceil(width / self.resolution)
+        arcpy.AddMessage(f'Padding columns: {paddingColumns}')
+        paddingRows = math.ceil(height / self.resolution)
+        arcpy.AddMessage(f'Padding rows: {paddingRows}')
         numberOfColumns += paddingColumns * 2
-        numberOfRows += paddingRrows * 2
-        start.X = start.X - paddingColumns * self.resolution
-        start.Y = start.Y - paddingRrows * self.resolution
+        numberOfRows += paddingRows * 2
 
         # compute start and end point indexes for further calculations
-        originalStartIndex = (numberOfRows - paddingRrows - 1, paddingColumns)
-        originalEndIndex = (paddingRrows, numberOfColumns - paddingColumns - 1)
+        if start.X < end.X and start.Y < end.Y: # from bottom left to top right
+            newStart = arcpy.Point(start.X - paddingColumns * self.resolution, start.Y - paddingRows * self.resolution)
+            newEnd = arcpy.Point(end.X + paddingColumns * self.resolution, end.Y + paddingRows * self.resolution)
+            originalStartIndex = (numberOfRows - paddingRows - 1, paddingColumns)
+            originalEndIndex = (paddingRows, numberOfColumns - paddingColumns - 1)
+            startArray = arcpy.RasterToNumPyArray(self.raster, newStart, numberOfColumns, numberOfRows)
+        elif start.X < end.X and start.Y > end.Y: # from top left to bottom right
+            newStart = arcpy.Point(start.X - paddingColumns * self.resolution, start.Y + paddingRows * self.resolution)
+            newEnd = arcpy.Point(end.X + paddingColumns * self.resolution, end.Y - paddingRows * self.resolution)
+            originalStartIndex = (paddingRows, paddingColumns)
+            originalEndIndex = (numberOfRows - paddingRows - 1, numberOfColumns - paddingColumns - 1)
+            startArray = arcpy.RasterToNumPyArray(self.raster, arcpy.Point(newStart.X, newEnd.Y), numberOfColumns, numberOfRows)
+        elif start.X > end.X and start.Y > end.Y: # from top right to bottom left
+            newEnd = arcpy.Point(end.X - paddingColumns * self.resolution, end.Y - paddingRows * self.resolution)
+            originalStartIndex = (paddingRows, numberOfColumns - paddingColumns - 1)
+            originalEndIndex = (numberOfRows - paddingRows - 1, paddingColumns)
+            startArray = arcpy.RasterToNumPyArray(self.raster, newEnd, numberOfColumns, numberOfRows)
+        else: # from bottom right to top left
+            newStart = arcpy.Point(start.X + paddingColumns * self.resolution, start.Y - paddingRows * self.resolution)
+            newEnd = arcpy.Point(end.X - paddingColumns * self.resolution, end.Y + paddingRows * self.resolution)
+            originalStartIndex = (numberOfRows - paddingRows - 1, numberOfColumns - paddingColumns - 1)
+            originalEndIndex = (paddingRows, paddingColumns)
+            startArray = arcpy.RasterToNumPyArray(self.raster, arcpy.Point(newEnd.X, newStart.Y), numberOfColumns, numberOfRows)
+        
 
-        startArray = arcpy.RasterToNumPyArray(self.raster, start, numberOfColumns, numberOfRows)
+        
 
         return startArray > -110, originalStartIndex, originalEndIndex
+    
+    def createPointArray(self, start: arcpy.Point, path: list, startIndexes):
+        pointArray = arcpy.Array()
+        tempPoint = copy.copy(start)
+        pointArray.add(start)
+        prevY, prevX = startIndexes
+        for i in range(len(path)):
+            y, x = path[i]
+            if y - prevY == 1: #down
+                tempPoint = arcpy.Point(tempPoint.X, tempPoint.Y - self.resolution)
+            elif y - prevY == -1: #up
+                tempPoint = arcpy.Point(tempPoint.X, tempPoint.Y + self.resolution)
+            elif x - prevX == 1: #right
+                tempPoint = arcpy.Point(tempPoint.X + self.resolution, tempPoint.Y)
+            elif x - prevX == -1: #left
+                tempPoint = arcpy.Point(tempPoint.X - self.resolution, tempPoint.Y)
+            
+            pointArray.add(tempPoint)
+            prevY, prevX = path[i]
+        
+        return pointArray
+
 
 def heuristic(a, b):
     # Manhattan distance
@@ -173,3 +245,14 @@ def a_star(grid, start, end):
                     heapq.heappush(open_set, (f_score[neighbor], neighbor))
 
     return None  # No path found
+
+def snapPointToRasterCenter(self, point: arcpy.Point):
+    #lower left corner of raster
+    rasterOriginX, rasterOriginY = self.raster.extent.XMin, self.raster.extent.YMax
+    #cell indexes of start point
+    col = int((point.X - rasterOriginX) / self.resolution)
+    row = int((rasterOriginY - point.Y) / self.resolution)
+    snapped_x = rasterOriginX + col * self.resolution + self.resolution / 2 - 100
+    snapped_y = rasterOriginY - row * self.resolution - self.resolution / 2 - 100
+
+    return arcpy.Point(snapped_x, snapped_y)
